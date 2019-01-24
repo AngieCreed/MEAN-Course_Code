a7acd10afb012d337f10d1e64b3c64444c1c8cf7
Angie Creed <angie.creed15@gmail.com>
2019-01-24 10:10:44 -0500

    more adds - validation

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,6 @@
 module.exports = {
     mongoURI:
-    'mongodb://angie:face29@ds211265.mlab.com:11265/creeddev'
+    'mongodb://angie:face29@ds211265.mlab.com:11265/creeddev',
+
+    secretOrKey: 'secret'
 };
